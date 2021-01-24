943874b8a8e52065d3ab95c9d5d279c82dcd89a2
clement571 <44236200+clement571@users.noreply.github.com>
2021-01-23 21:58:44 -0600

    Update spdimCanvasAccountLoad.sql

--- spdimCanvasAccountLoad.sql
+++ spdimCanvasAccountLoad.sql
@@ -9,7 +9,7 @@ GO
 ALTER PROCEDURE [dbo].[spdimCanvasAccountLoad]
 AS
 --add this comment on 1/23/21
-
+--edit on github 1/23/21
 WITH cteCanvasAccount AS (
 	SELECT DISTINCT
 	        CAST(account_dim.id as bigint) AS CanvasSurrogateId,
